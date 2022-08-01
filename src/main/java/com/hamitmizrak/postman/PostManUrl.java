2f598b87c869f27dfadd8b3099d5903ddf67e732
hamitmizrak <hamitmizrak@gmail.com>
2022-08-01 19:42:24 +0300

    Postman URL

---
+++ src/main/java/com/hamitmizrak/postman/PostManUrl.java
@@ -0,0 +1,37 @@
+package com.hamitmizrak.postman;
+
+
+public class PostManUrl {
+
+    //ROOT
+    // http://localhost:8080/api/v1
+    // http://localhost:8080/api/v1/index
+
+    //DATA ALL CREATE 10
+    // http://localhost:8080/api/v1/employees/alldata
+
+
+    //CREATE
+    // http://localhost:8080/api/v1/employees
+    //    {
+    //        "employeeName":"Hamit",
+    //            "employeeSurname":"Mızrak"
+    //    }
+
+    //LIST
+    // http://localhost:8080/api/v1/employees
+
+    //FIND
+    // http://localhost:8080/api/v1/employees/1
+
+    //DELETE
+    // http://localhost:8080/api/v1/employees/1
+
+    //UPDATE
+    // http://localhost:8080/api/v1/employees/1
+    //    {
+    //        "employeeName":"Hamit",
+    //            "employeeSurname":"Mızrak"
+    //    }
+
+}
