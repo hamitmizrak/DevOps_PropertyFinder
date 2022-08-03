eba7e339ec90017a013268d48afa51dc3fbb0880
hamitmizrak <hamitmizrak@gmail.com>
2022-08-03 16:52:01 +0300

    Spring Boot Dockerize add

--- src/main/java/com/hamitmizrak/bean/EmployeeBean.java
+++ src/main/java/com/hamitmizrak/bean/EmployeeBean.java
@@ -2,20 +2,15 @@ package com.hamitmizrak.bean;
 
 import com.hamitmizrak.data.entity.EmployeeEntity;
 import com.hamitmizrak.data.repository.IEmployeeRepository;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.logging.Logger;
 
 @Configuration
 public class EmployeeBean {
 
     @Autowired
     IEmployeeRepository repository;
-
-
 
     @Bean
     public void getAllData(){
---
+++ src/main/java/com/hamitmizrak/postman/PostManUrl.java
@@ -0,0 +1,36 @@
+package com.hamitmizrak.postman;
+
+
+public class PostManUrl {
+
+    //ROOT
+    // http://localhost:8080/api/v1
+    // http://localhost:8080/api/v1/index
+
+//### Postmen
+
+//            LIST
+//    http://localhost:8080/api/v1/employees
+//
+//    FIND
+//    http://localhost:8080/api/v1/employees/1
+//
+//    DELETE
+//    http://localhost:8080/api/v1/employees/1
+//
+//    CREATE
+//    http://localhost:8080/api/v1/employees
+//    {
+//        "employeeName":"Hamit",
+//            "employeeSurname":"Mızrak"
+//    }
+//
+//
+//    UPDATE
+//    http://localhost:8080/api/v1/employees/1
+//    {
+//        "employeeName":"Hamit",
+//            "employeeSurname":"Mızrak"
+//    }
+
+}
